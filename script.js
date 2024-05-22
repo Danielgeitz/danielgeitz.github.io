4e741dbbe08e146cde2f345ce2f2dbc5357a3fdd
Daniel Geitz <65306972+Danielgeitz@users.noreply.github.com>
2024-05-23 01:34:31 +0200

    testing navigation buttons on expanded card

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ function expandCard(card) {
 function closeOverlay() {
     document.getElementById('overlay').classList.remove('show');
 }
+
+var currentProjectIndex = 0;
+var projects = document.querySelectorAll('.card');
+
+function expandCard(card) {
+    currentProjectIndex = Array.from(projects).indexOf(card);
+    updateOverlayContent(card);
+    document.getElementById('overlay').classList.add('show');
+}
+
+function updateOverlayContent(card) {
+    document.getElementById('overlay-image').src = card.querySelector('img').src;
+    document.getElementById('overlay-title').innerText = card.querySelector('h3').innerText;
+    document.getElementById('overlay-description').innerText = card.querySelector('p').innerText;
+}
+
+function closeOverlay() {
+    document.getElementById('overlay').classList.remove('show');
+}
+
+function prevProject() {
+    currentProjectIndex = (currentProjectIndex - 1 + projects.length) % projects.length;
+    updateOverlayContent(projects[currentProjectIndex]);
+}
+
+function nextProject() {
+    currentProjectIndex = (currentProjectIndex + 1) % projects.length;
+    updateOverlayContent(projects[currentProjectIndex]);
+}
