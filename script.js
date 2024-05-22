9da2873daee12d763c8439cfda8546714be0c4d7
Daniel Geitz <65306972+Danielgeitz@users.noreply.github.com>
2024-05-23 01:24:57 +0200

    bug fix expanded card view

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function expandCard(card) {
     document.getElementById('overlay-image').src = card.querySelector('img').src;
     document.getElementById('overlay-title').innerText = card.querySelector('h3').innerText;
     document.getElementById('overlay-description').innerText = card.querySelector('p').innerText;
-    document.getElementById('overlay').style.display = 'flex';
+    document.getElementById('overlay').classList.add('show');
 }
 
 function closeOverlay() {
-    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('overlay').classList.remove('show');
 }
